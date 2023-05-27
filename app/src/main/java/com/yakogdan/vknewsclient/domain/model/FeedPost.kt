cbe5661d5112a8ba409cebc213e2336b136b7eaf
yakogdan <yakogdan@gmail.com>
2023-05-27 16:36:09 +0800

    lesson_4.9

--- app/src/main/java/com/yakogdan/vknewsclient/domain/model/FeedPost.kt
+++ app/src/main/java/com/yakogdan/vknewsclient/domain/model/FeedPost.kt
@@ -3,6 +3,7 @@ package com.yakogdan.vknewsclient.domain.model
 import com.yakogdan.vknewsclient.R
 
 data class FeedPost(
+    val id: Int = 0,
     val communityName: String = "/dev/null",
     val publicationDate: String = "14:00",
     val avatarResId: Int = R.drawable.post_comunity_thumbnail,
--- app/src/main/java/com/yakogdan/vknewsclient/ui/VkNewsMainScreen.kt
+++ app/src/main/java/com/yakogdan/vknewsclient/ui/VkNewsMainScreen.kt
@@ -1,25 +1,38 @@
 package com.yakogdan.vknewsclient.ui
 
-import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yakogdan.vknewsclient.MainViewModel
-import com.yakogdan.vknewsclient.domain.model.FeedPost
+import com.yakogdan.vknewsclient.ui.theme.VkNewsClientTheme
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
 
@@ -53,15 +66,85 @@ fun MainScreen(viewModel: MainViewModel) {
                 }
             }
         }
-    ) {
-        val feedPost = viewModel.feedPost.observeAsState(FeedPost())
-        PostCard(
-            modifier = Modifier.padding(8.dp),
-            feedPost = feedPost.value,
-            onViewsClickListener = viewModel::updateCount,
-            onShareClickListener = viewModel::updateCount,
-            onCommentClickListener = viewModel::updateCount,
-            onLikeClickListener = viewModel::updateCount
-        )
+    ) { paddingValues ->
+        VkScreen(viewModel = viewModel, paddingValues = paddingValues)
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun VkScreen(viewModel: MainViewModel, paddingValues: PaddingValues) {
+    VkNewsClientTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+        ) {
+            val models = viewModel.feedPosts.observeAsState(listOf())
+            LazyColumn(
+                modifier = Modifier.padding(paddingValues), contentPadding = PaddingValues(
+                    top = 16.dp,
+                    start = 8.dp,
+                    end = 8.dp,
+                    bottom = 72.dp
+                ),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(models.value, key = { it.id }) { feedPost ->
+                    val dismissState = rememberDismissState()
+                    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                        viewModel.removeItem(feedPost)
+                    }
+                    SwipeToDismiss(
+                        modifier = Modifier.animateItemPlacement(),
+                        state = dismissState,
+                        directions = setOf(DismissDirection.EndToStart),
+                        background = {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(16.dp)
+                                    .background(Color.Red.copy(alpha = 0.5f)),
+                                contentAlignment = Alignment.CenterEnd
+                            ) {
+                                Text(
+                                    modifier = Modifier.padding(16.dp),
+                                    text = "Delete item",
+                                    color = Color.White,
+                                    fontSize = 24.sp
+                                )
+                            }
+                        }) {
+                        PostCard(
+                            feedPost = feedPost,
+                            onLikeClickListener = { statisticItem ->
+                                viewModel.updateCount(
+                                    feedPost = feedPost,
+                                    item = statisticItem
+                                )
+                            },
+                            onShareClickListener = { statisticItem ->
+                                viewModel.updateCount(
+                                    feedPost = feedPost,
+                                    item = statisticItem
+                                )
+                            },
+                            onViewsClickListener = { statisticItem ->
+                                viewModel.updateCount(
+                                    feedPost = feedPost,
+                                    item = statisticItem
+                                )
+                            },
+                            onCommentClickListener = { statisticItem ->
+                                viewModel.updateCount(
+                                    feedPost = feedPost,
+                                    item = statisticItem
+                                )
+                            }
+                        )
+                    }
+                }
+            }
+        }
     }
 }
